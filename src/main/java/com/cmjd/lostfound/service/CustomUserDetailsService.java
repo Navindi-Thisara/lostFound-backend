b959dede283525484f4cc3efc598f14751ea0c1c
Navindi-Thisara <navindithisara214@gmail.com>
2025-05-07 14:03:45 -0300

    Refactor CustomUserDetailsService for improved user detail retrieval

--- src/main/java/com/cmjd/lostfound/service/CustomUserDetailsService.java
+++ src/main/java/com/cmjd/lostfound/service/CustomUserDetailsService.java
@@ -1,4 +1,24 @@
 package com.cmjd.lostfound.service;
 
-public class CustomUserDetailsService {
+import com.cmjd.lostfound.model.User;
+import com.cmjd.lostfound.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
 }
